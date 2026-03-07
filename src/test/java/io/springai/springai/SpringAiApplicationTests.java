86ae2dabfb81307373963776fc6261d89584936b
jkchappydev <jkchappydev@gmail.com>
2026-03-07 18:48:47 +0900

    init: spring-ai project

---
+++ src/test/java/io/springai/springai/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springai.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
